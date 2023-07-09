ca6406294f6f1b667922583ea0cdcb83b7379dc1
biaaPrado <104280744+biaaPrado@users.noreply.github.com>
2023-07-08 22:20:03 -0300

    inserindo tailwind, e views basicas

---
+++ gymhunt-web/resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.site')
+@section('titulo', 'GymHunt - Home')
+@section('content') 
+
+<body class="bg-gradient-to-t from-indigo-500 via-indigo-400 to-indigo-300 my-24 md:my-30 lg:my-24">
+    <div class="flex m-10 items-center">
+        <div class="py-10">
+            <h1 class="text-5xl font-medium font-sans ">Conheça nosso sistema, treine no academia mais próxima à você, em um ambiente de melhor qualidade!</h1>
+
+            <a href="#"> 
+                <button class="text-2xl p-3 bg-gradient-to-r from-indigo-600 to-gray-400 opacity-80 border-solid border-zinc-400 border-1 my-5 rounded-full">
+                    Explore o mapa da sua região <i class="fa-solid fa-map-location-dot"></i> 
+                </button>
+            </a>
+        </div>
+
+        <div class="">
+            <img src=".\img\mapa.png" alt="mapa ficticio" class="rounded-lg">
+        </div>
+    </div>
+</body> 
+@endsection
+
---
+++ gymhunt-web/resources/views/layout/header.blade.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <!-- Icones Font Awesome -->
+        <script src="https://kit.fontawesome.com/189da14b09.js" crossorigin="anonymous"></script>
+        <title>@yield('titulo')</title>
+        <!-- Import tailwindCSS -->
+        @vite('resources/css/app.css')
+    </head>
+
+    <header class="h-16 md:h-30 lg:h-24 bg-gray-600 fixed inset-x-0 top-0 z-50 rounded-b-lg">
+        <nav class="flex items-center justify-between p-3 m-1" aria-label="Global">
+            <div class="flex items-center">
+                <div class="flex">
+                    <a href="/" class="-m-1.5 p-1.5">
+                        <img src="\img\logoGymHunt.png" alt="" class="h-16 w-auto">
+                    </a>
+                </div>
+                
+                <div class="ml-10 md:block">
+                    <div class="hidden lg:flex items-baseline space-x-4 lg:gap-x-12 m-2">
+                        <a href="#" class="text-2xl font-semibold leading-6 hover:text-white rounded-md px-3 py-2 hover:bg-gray-700 text-gray-400">Product</a>
+                        <a href="#" class="text-2xl font-semibold leading-6 hover:text-white rounded-md px-3 py-2 hover:bg-gray-700 text-gray-400">Features</a>
+                        <a href="#" class="text-2xl font-semibold leading-6 hover:text-white rounded-md px-3 py-2 hover:bg-gray-700 text-gray-400">Marketplace</a>
+                        <a href="#" class="text-2xl font-semibold leading-6 hover:text-white rounded-md px-3 py-2 hover:bg-gray-700 text-gray-400">Company</a>
+                    </div>
+                </div>
+
+            </div>  
+            
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+                <a href="login.index" class="text-2xl font-semibold leading-6 text-gray-400">Log in <i class="fa-solid fa-right-to-bracket fa-xl"></i></a>
+            </div>
+        </nav>
+    </header>
