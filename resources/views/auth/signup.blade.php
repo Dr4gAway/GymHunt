7f3229c2bcff4c6f552d2b51a11170ed228ee476
Gabriele de Lima <ra2157100@CTIBAURU.FEB.UNESP.BR>
2023-07-26 08:47:20 -0300

    Ajuste da tea de cadastro

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -6,10 +6,10 @@
     
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img class="mx-auto w-auto" src=".\img\logoIcon.png" alt="Your Company">
-            <h2 class="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Faça login para entrar na sua conta!</h2>
+            <h2 class="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"></h2>
         </div>
 
-        <div class="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+        <!-- <div class="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
             
             <form class="space-y-6" action="{{route('signup')}}" method="POST">
 
@@ -30,7 +30,90 @@
                     <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 p-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cadastre-se</button>
                 </div>
             </form>
+        </div> -->
+    </div>
+
+    <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
+            <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
         </div>
+        <div class="mx-auto max-w-2xl text-center">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Faça seu cadastro</h2>
+            <p class="mt-2 text-lg leading-8 text-gray-600">Ao realizar seu cadastro, você poderá explorar nosso site da melhor maneira!</p>
+        </div>
+        
+        <form action="{{route('signup')}}" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+            @csrf
+            @method('POST')
+
+            @error('invalid_credentials')
+                <p class="text-red-500"> {{$message}} </p>   
+            @enderror
+
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+                    <label for="name" class="block text-sm font-semibold leading-6 text-gray-900">Primeiro nome</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="name" id="name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                
+                <div>
+                    <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                
+                <div class="sm:col-span-2">
+                    <label for="phone" class="block text-sm font-semibold leading-6 text-gray-900">Telefone</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="phone" id="telefone" placeholder="ex: +55 (XX) XXXXX-XXXX"  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                
+                <div class="sm:col-span-2">
+                    <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                    <div class="mt-2.5">
+                        <input type="email" name="email" id="email" placeholder="ex: seuemail@gmail.com.br" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                    <label for="password" class="block text-sm font-semibold leading-6 text-gray-900">Senha</label>
+                    <div class="mt-2.5">
+                        <input type="password" name="password" id="password"  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                    <label for="password_confirmation" class="block text-sm font-semibold leading-6 text-gray-900">Confirme sua senha</label>
+                    <div class="mt-2.5">
+                        <input type="email" name="password_confirmation" id="password_confirmation" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="flex gap-x-4 sm:col-span-2">
+                    <div class="flex h-6 items-center">
+                    <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
+                        <button type="button" class="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="false" aria-labelledby="switch-1-label">
+                            <span class="sr-only">Agree to policies</span>
+                            <!-- Enabled: "translate-x-3.5", Not Enabled: "translate-x-0" -->
+                            <span aria-hidden="true" class="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
+                        </button>
+                    </div>
+
+                    <label class="text-sm leading-6 text-gray-600" id="switch-1-label">
+                        Ao selecionar isso, você concorda com nossa
+                        <a href="#" class="font-semibold text-indigo-600">política de privacidade</a>.
+                    </label>
+                </div>
+            </div>
+
+            <div class="mt-10">
+                <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cadastre-se</button>
+            </div>
+        </form>
     </div>
 
 @endsection
