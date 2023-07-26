ad8edc9480561d500d5d16fd9fed18fcd33f432d
Dr4gAway <gilmar.filho@unesp.br>
2023-07-26 04:10:27 -0300

    refactor: added post create css

--- resources/views/livewire/post/create.blade.php
+++ resources/views/livewire/post/create.blade.php
@@ -1,9 +1,24 @@
-<div class="w-full flex flex-col">
-    Testando muito foda
-    {{ $body }}
-    <textarea wire:model="body" class="resize-none"></textarea>
-    @error('body')
-        {{$message}}
-    @enderror
-    <button wire:click="store" class="">Enviar</button>
+<div class="w-full flex gap-4 bg-white p-4 rounded-2xl">
+    <span class="rounded-full h-10 w-10 bg-blue-500"></span>
+
+    <div class="flex w-full gap-4">
+        <textarea wire:model="body" rows="1"
+                class=" border-4 border-gymhunt-purple-1
+                        focus:outline-none focus:ring ring-gymhunt-purple-2
+                        placeholder:text-gymhunt-purple-2
+                        rounded-lg px-5 py-3 w-full
+                        resize-none"
+                placeholder="Como vai seu treino?">
+        </textarea>
+        
+        
+        <button wire:click="store" class="">
+
+            @php
+               echo file_get_contents("img/icons/send-icon.svg") 
+            @endphp
+           {{--  <img src="img\icons\send-icon.svg" alt="botão enviar" class="text-gymhunt-purple-1"> --}}
+        </button>
+    </div>
+    
 </div>
