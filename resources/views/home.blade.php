fae85a1425a7df80cd487976037e58712d76a262
Gabriele de Lima <beatriz.p.soche@unesp.br>
2023-08-07 10:40:52 -0300

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,29 @@
 @section('titulo', 'GymHunt - Home')
 @section('content')
 
-<div class="flex flex-col justify-center my-12 px-6 py-4 lg:px-8">
+<div class="flex flex-col justify-center my-12">
+    <div class="flex">
+        <div class="p-12 space-y-8">
+            <div class="spcae-y-2">
+                <p class="text-8xl font-sans font-bold">Treine</p>
+                <p class="text-8xl font-sans font-bold">em qualquer lugar</p>
+            </div>
 
-    <div class="px-6 py-8 sm:py-8 lg:px-8 flex">
-        <div class="py-10 space-y-8">
-            <h1 class="text-8xl font-sans font-bold">Treine em qualquer lugar</h1>
-            <p class="text-4xl font-sans font-semibold">Conheça o nosso sistema e treine na academia mais próxima com a melhor qualidade</p>
+            <p class="text-4xl font-sans font-semibold w-3/5">Conheça o nosso sistema e treine na academia mais próxima com a melhor qualidade</p>
+            
             <a href="#"> 
-                <button class="text-2xl p-3 text-white uppercase bg-gymhunt-purple-2 border-solid border-zinc-400 border-1 my-5 rounded-lg">
-                    Explore o mapa <b>da sua região</b> <i class="fa-solid fa-map-location-dot"></i> 
+                <button class="flex space-x-2 text-3xl p-7 tracking-widest text-white uppercase bg-gymhunt-purple-2 border-solid border-zinc-400 border-1 my-10 rounded-lg">
+                    <i class="fa-solid fa-map-location-dot fa-2x"></i>
+                    <div>
+                        <p>Explore o mapa</p>
+                        <b class="font-extrabold tracking-widest">da sua região</b>
+                    </div>   
                 </button>
             </a>
         </div>
 
-        <div class="">  
-            <img src=".\img\mapa.png" alt="mapa ficticio" class="rounded-lg">
+        <div class="absolute top-0 z-[-1]">  
+            <img src=".\img\ways-sided.png" width="5000px" alt="mapa ficticio" class="rounded-lg">
         </div>
     </div>
     
