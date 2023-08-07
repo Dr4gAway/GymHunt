37ba431a231b1abfa3afc9500e191e00fa3744eb
Dr4gAway <gilmar.filho@unesp.br>
2023-08-07 20:13:03 -0300

    refactor: adjusting indent size

--- resources/views/livewire/comment/create.blade.php
+++ resources/views/livewire/comment/create.blade.php
@@ -1,19 +1,20 @@
 <div class="flex w-full gap-4">
-    <textarea wire:model="body" rows="1"
-            class=" border-4 border-gymhunt-purple-1
-                    focus:outline-none focus:ring ring-gymhunt-purple-2
-                    placeholder:text-gymhunt-purple-2
-                    rounded-lg px-5 py-3 w-full
-                    resize-none"
-            placeholder="Como vai seu treino?">
+    <textarea wire:model="body" 
+	    class=" border-4 border-gymhunt-purple-1
+		    focus:outline-none focus:ring ring-gymhunt-purple-2
+		    placeholder:text-gymhunt-purple-2
+		    rounded-lg px-5 py-3 w-full
+		    resize-none"
+	    placeholder="Como vai seu treino?"
+	    id="comment-{{$post_id}}">
     </textarea>
-    
-    
-    <button wire:click="store" class="">
 
-        @php
-            echo file_get_contents("img/icons/send-icon.svg") 
-        @endphp
-        {{--  <img src="img\icons\send-icon.svg" alt="botão enviar" class="text-gymhunt-purple-1"> --}}
-        </button>
+    <button wire:click="store" class="">
+	@php
+	    echo file_get_contents("img/icons/send-icon.svg") 
+	@endphp
+	{{--  <img src="img\icons\send-icon.svg" alt="botão enviar" class="text-gymhunt-purple-1"> --}}
+    </button>
+
+    
 </div>
--- resources/views/livewire/comment/view.blade.php
+++ resources/views/livewire/comment/view.blade.php
@@ -1,5 +1,5 @@
 
 <div class="flex flex-col items-centerflex-col gap-1">
     <p class="font-semibold">{{$comment->user->name}}</p>
-     <p class="grow font-medium">{{ $comment->body }}</p>
+    <p class="grow font-medium">{{ $comment->body }}</p>
 </div>
