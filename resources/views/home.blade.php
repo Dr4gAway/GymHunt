b4106a17aee6515dc8af639486f3649ea98a3886
Gabriele de Lima <beatriz.p.soche@unesp.br>
2023-08-08 15:25:05 -0300

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,12 +27,35 @@
             <img src=".\img\ways-sided.png" width="5000px" alt="mapa ficticio" class="rounded-lg">
         </div>
     </div>
+
+    <div class="flex bg-blue-500">
+        <div class="p-12 space-y-8">
+            <div class="spcae-y-2">
+                <p class="text-8xl font-sans font-bold">Treine</p>
+                <p class="text-8xl font-sans font-bold">em qualquer lugar</p>
+            </div>
+
+            <p class="text-4xl font-sans font-semibold w-3/5">Conheça o nosso sistema e treine na academia mais próxima com a melhor qualidade</p>
+            
+            <a href="#"> 
+                <button class="flex space-x-2 text-3xl p-7 tracking-widest text-white uppercase bg-gymhunt-purple-2 border-solid border-zinc-400 border-1 my-10 rounded-lg">
+                    <i class="fa-solid fa-map-location-dot fa-2x"></i>
+                    <div>
+                        <p>Explore o mapa</p>
+                        <b class="font-extrabold tracking-widest">da sua região</b>
+                    </div>   
+                </button>
+            </a>
+        </div>
+
+    
+    </div>
     
     <div class="self-center">
         <livewire:counter />
     </div>
 
-</section>
+</div>
 
 @endsection
 
