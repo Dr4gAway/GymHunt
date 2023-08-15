66ee9413700d14c04b1d8f53ca6e4b6c9ffdbf09
Gilmar dos Santos Filho <gimar.filho@unesp.com>
2023-08-15 14:41:30 -0300

    fix: display in comment component

--- resources/views/livewire/comment/view.blade.php
+++ resources/views/livewire/comment/view.blade.php
@@ -1,10 +1,14 @@
-<div class="flex items-centerflex-col gap-2">
-    <span class="flex-none rounded-full h-10 w-10 bg-red-500"></span>
-    <div class="flex flex-col gap-0">
-        <p  class="font-semibold">{{$comment->user->name}}</p>
-        <p class="grow font-medium">{{ $comment->body }}</p>
+<div class="flex items-start justify-between gap-2">
+    <div class="flex gap-2">
+        <span class="flex-none rounded-full h-10 w-10 bg-red-500"></span>
+        <div class="flex flex-col gap-0">
+            <p  class="font-semibold">{{$comment->user->name}}</p>
+            <p class="grow font-medium">{{ $comment->body }}</p>
+        </div>
     </div>
 
-    {{$likesCount}}
-    <img src="{{ $liked ? '\img\icons\heart-selected-icon.svg' : '\img\icons\heart-icon.svg' }}" wire:click="handleLike" alt="like" class="h-5">
+    <div class="flex items-center gap-2">
+        {{$likesCount}}
+        <img src="{{ $liked ? '\img\icons\heart-selected-icon.svg' : '\img\icons\heart-icon.svg' }}" wire:click="handleLike" alt="like" class="cursor-pointer h-5">
+    </div>
 </div>
