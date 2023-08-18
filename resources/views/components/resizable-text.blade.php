5d0d0972abd54ca931b256538c7df41360a278e4
Dr4gAway <gilmar.filho@unesp.br>
2023-08-17 22:46:57 -0300

    feat: added resizable textarea component

---
+++ resources/views/components/resizable-text.blade.php
@@ -0,0 +1,22 @@
+@props([
+    'model' => null,
+    'placeholder' => 'placeholder',
+    'customClasses' => []
+])
+
+<div class="w-full">
+    <textarea
+        x-data="{ resize: () => { $el.style.height = 'auto'; $el.style.height = ($el.scrollHeight + 5) + 'px' } }"
+        wire:model="{{ $model }}"
+        x-init="resize()"
+        @input="resize()"
+        wire:ignore
+        placeholder="{{ $placeholder }}"
+        class=" border-4 border-gymhunt-purple-1
+                focus:outline-none focus:ring ring-gymhunt-purple-2
+                placeholder:text-gymhunt-purple-2
+                rounded-lg px-5 py-3 w-full resize-none overflow-hidden"
+        >
+    </textarea>
+
+</div>
--- resources/views/livewire/post/create.blade.php
+++ resources/views/livewire/post/create.blade.php
@@ -2,22 +2,15 @@
     <span class="rounded-full h-10 w-10 bg-blue-500"></span>
 
     <div class="flex w-full gap-4">
-        <textarea wire:model="body" rows="1"
-                class=" border-4 border-gymhunt-purple-1
-                        focus:outline-none focus:ring ring-gymhunt-purple-2
-                        placeholder:text-gymhunt-purple-2
-                        rounded-lg px-5 py-3 w-full
-                        resize-none"
-                placeholder="Como vai seu treino?">
-        </textarea>
-        
+
+        <x-resizable-text model="body" customClasses="['w-full']" placeholder="Como vai seu treino?" />
         
         <button wire:click="store" class="">
 
             @php
                echo file_get_contents("img/icons/send-icon.svg") 
             @endphp
-           {{--  <img src="img\icons\send-icon.svg" alt="botão enviar" class="text-gymhunt-purple-1"> --}}
+           
         </button>
     </div>
     
