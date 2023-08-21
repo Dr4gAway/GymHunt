97fe7abf29ec181bf0ba98b1bdf5b712e547b6ff
Gabriele de Lima <beatriz.p.soche@unesp.br>
2023-08-21 10:43:00 -0300

    hover no botao da home

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -40,7 +40,6 @@
         
         <form action="{{route('signup')}}" method="POST" class="mx-auto mt-10 max-w-xl sm:mt-14">
             @csrf
-            @method('POST')
 
             @error('invalid_credentials')
                 <p class="text-red-500"> {{$message}} </p>   
@@ -54,12 +53,12 @@
                     </div>
                 </div>
                 
-                <div>
+                <!-- <div>
                     <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Apelido</label>
                     <div class="mt-2.5">
                         <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
-                </div>
+                </div> -->
                 
                 <div class="sm:col-span-2">
                     <label for="phone" class="block text-sm font-semibold leading-6 text-gray-900">Telefone</label>
@@ -89,7 +88,7 @@
                     </div>
                 </div>
 
-                <div class="sm:col-span-2 space-y-3">
+                <!-- <div class="sm:col-span-2 space-y-3">
                     <label for="password" class="block text-sm font-semibold leading-6 text-gray-900">Insira suas imagens</label>
                     <div class="sm:col-span-2">
                         <label for="banner" class="block text-sm font-semibold leading-6 text-gray-900">1. Banner</label> 
@@ -122,7 +121,7 @@
                     </div>
                 </div>
 
-                <!-- para users -->
+                <!-- para users 
                 se for usuario 
                 <div class="sm:col-span-2">
                     <label for="cpf" class="block text-sm font-semibold leading-6 text-gray-900">CPF</label>
@@ -138,7 +137,7 @@
                     </div>
                 </div>
 
-                <!-- para academias -->
+                <!-- para academias 
                 se for academia
                 <div class="sm:col-span-2">
                     <label for="cnpj" class="block text-sm font-semibold leading-6 text-gray-900">CNPJ</label>
@@ -171,10 +170,10 @@
 
                 <div class="flex gap-x-4 sm:col-span-2">
                     <div class="flex h-6 items-center">
-                    <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
+                    <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" 
                         <button type="button" class="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="false" aria-labelledby="switch-1-label">
                             <span class="sr-only">Agree to policies</span>
-                            <!-- Enabled: "translate-x-3.5", Not Enabled: "translate-x-0" -->
+                            <!-- Enabled: "translate-x-3.5", Not Enabled: "translate-x-0" 
                             <span aria-hidden="true" class="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
                         </button>
                     </div>
@@ -184,7 +183,7 @@
                         <a href="#" class="font-semibold text-indigo-600">política de privacidade</a>.
                     </label>
                 </div>
-            </div>
+            </div> -->
 
             <div class="mt-10">
                 <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cadastre-se</button>
