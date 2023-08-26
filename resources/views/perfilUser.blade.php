4739ec79fe11aaa2371e467de82c328859f73895
Gabriele de Lima <beatriz.p.soche@unesp.br>
2023-08-26 08:49:00 -0300

    view perfil header

--- resources/views/perfilUser.blade.php
+++ resources/views/perfilUser.blade.php
@@ -5,9 +5,35 @@
     <div class="">
         <img class="w-full" src=".\img\banner.png" alt="">
     </div>
-    <div class=" w-full">
-        <div class="bg-red-500 absolute left-2 w-4 h-4">a</div>
+    
+    <div class="w-full flex flex-row">
+        <img class="w-64" src=".\img\avatar.png" alt="">
+
+        <div class="w-full flex flex-row justify-between ">
+            <div class="m-4 space-y-2">
+                <div class="flex flex-rol items-end space-x-6 font-poppins">
+                    <h3 class="font-bold text-3xl">Gilmar dos Santos</h3>
+                    <h6 class="text-lg">@gilmass</h6>
+                </div>
+
+                <div class="flex flex-rol items-end space-x-6 font-poppins">
+                    <p>400 seguidores</p>
+                    <p>698 seguindo</p>
+                </div>
+            </div>
+    
+            <div class="flex flex-rol font-poppins text-white space-x-3 my-4 mx-6">
+                <div>
+                    <button class="bg-gymhunt-purple-2 text-lg py-2 px-3 rounded-2xl">. . .</button>
+                </div>
+
+                <div>
+                    <button class="bg-gymhunt-purple-2 text-lg py-2 px-3 rounded-2xl">Seguir</button>
+                </div>
+            </div>
+        </div>
         
+
     </div>
 </div>
 @endsection
