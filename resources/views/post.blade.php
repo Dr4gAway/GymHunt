e0851f95a302749b9491b8873c6dd4de0879f392
Dr4gAway <gilmar.filho@unesp.br>
2023-09-08 19:24:23 -0300

    fix: carousel in posts single page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,27 @@
 @extends('layout.site')
 @section('titulo', 'GymHunt - teste')
 @section('content')
-    <section class="flex justify-center w-full my-8">
+    <section class="flex justify-center w-full my-8" x-data="{
+        imageOpen: false,
+
+        disableScroll() {
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+            window.onscroll = () => {
+                window.scrollTo(scrollLeft, scrollTop);
+            }
+        },
+    
+        enableScroll() {
+            window.onscroll = function() {};
+        },
+    
+        imageOpen: false
+    }">
+
+        <div x-show="imageOpen">
+            <livewire:carousel  />
+        </div>
         <livewire:post :post="$post" :showAll="true" />
     </section>
 @endsection
