936a6562ab2db0ca5cedca17078af84313e70949
Dr4gAway <gilmar.filho@unesp.br>
2023-11-02 12:13:13 -0300

    fix: updated seeders for Common and Gym entities

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,8 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'phone' => fake()->e164PhoneNumber(),
             'about' => fake()->sentence(),
+            'avatar' => 'img/profile/default_avatar.jpeg',
+            'banner' => 'img/profile/default_banner.png',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
